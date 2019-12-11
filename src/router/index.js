f8c95f64f4f978df6c8fa7120bd9090d837ca67e
hyx <qq862753149@163.com>
2019-12-11 18:43:40 +0800

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -7,11 +7,6 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
       path: '/Home',
       name: 'Home',
       component: (resolve) => require(['@/page/Home'], resolve)
